b9f083f5cdae8ec370712c45b33abd7af650e84f
mayabridgewater <55682563+mayabridgewater@users.noreply.github.com>
2020-01-27 10:39:21 +0200

    delete/upload new images

--- realtor-server/db/images.js
+++ realtor-server/db/images.js
@@ -6,19 +6,22 @@ function postImages(id ,images) {
     for (let i = 0; i < images.length; i++) {
         if (i === images.length-1) {
             query += `(default, ?, ?)`;
-            params.push(id);
+            params.push(parseInt(id));
             params.push(images[i])
         } else {
             query += `(default, ?, ?), `;
-            params.push(id);
+            params.push(parseInt(id));
             params.push(images[i]);
         }
     } 
+    console.log(query, params)
     return new Promise((resolve, reject) => {
         connection.query(`insert into images (id, apartment_id, url) values ${query}`, params, function(error, results, fields) {
             if (error) {
+                console.log(error);
                 reject(error)
             } else {
+                console.log(results)
                 resolve(results)
             }
         })
@@ -49,8 +52,38 @@ function getImagesById(id) {
     })
 }
 
+function deleteImages(id, images) {
+    let query = `delete from images where id not in (`;
+    let params = [];
+    for (let i = 0; i < images.length; i++) {
+        if (images.length === 1) {
+            query += `?)`;
+            params.push(images[i])
+        }else if (i === 0) {
+            query += `?`;
+            params.push(images[i])
+        }else if (i === images.length - 1) {
+            query + `, ?)`;
+            params.push(images[i])
+        }else {
+            query += `, ?`;
+            params.push(images[i])
+        }
+    }
+    return new Promise((resolve, reject) => {
+        connection.query(query, params, function(error, results, fields) {
+            if (error) {
+                reject(error)
+            }else {
+                resolve(results)
+            }
+        })
+    })
+}
+
 module.exports = {
     postImages,
     getImagesById,
-    getAllImages
+    getAllImages,
+    deleteImages
 }
